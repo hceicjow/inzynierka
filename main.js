9cb521e9d19680499bca521f5f31d31e0e57a3ba
hceicjow <klimiuk.wojtek@gmail.com>
2021-06-04 13:54:43 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const bars = document.body.querySelector('.fa-bars')
+const times = document.body.querySelector('.fa-times')
+const navigation = document.body.querySelector('.navWrapper')
+
+bars.addEventListener('click', function () {
+    bars.style.display = 'none';
+    times.style.display = 'block'
+    navigation.style.top = '0';
+    document.body.style.overflowY = 'hidden'
+})
+times.addEventListener('click', function () {
+    bars.style.display = 'block';
+    times.style.display = 'none'
+    navigation.style.top = '-100vh';
+    document.body.style.overflowY = 'visible'
+})
